d74aac87ee52ec886c13c2f3a2984e26244e7270
aman-singh1601 <aman16singh2001@gmail.com>
2024-04-02 09:36:41 +0530

    added changes

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -9,8 +9,8 @@ const SocketContext = React.createContext<SocketContextType | null>(null);
 
 const SocketProvider = ({ children }: { children: React.ReactNode }) => {
 
-    // const socket = useMemo(() => io('https://face-auth-server-fsp0.onrender.com'),[]);
-    const socket = useMemo(() => io('http://localhost:6969'),[]);
+    const socket = useMemo(() => io('https://face-auth-server-fsp0.onrender.com'),[]);
+    // const socket = useMemo(() => io('http://localhost:6969'),[]);
 
 
     const socketContextValue: SocketContextType = {
